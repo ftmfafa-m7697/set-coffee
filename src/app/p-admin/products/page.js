eee6de057b415b626eee311b9af57ea258ee267f
ftm <dokhtaretabeston7697@gmail.com>
2025-08-20 14:55:51 +0330

    fixed bug add product

--- src/app/p-admin/products/page.js
+++ src/app/p-admin/products/page.js
@@ -1,32 +1,29 @@
 import connectToDB from "@/configs/db";
 import ProductModel from "@/models/Product";
 import AdminPanelLayout from "@/components/layouts/AdminPanelLayout";
-import styles from "@/components/templates/p-admin/products/table.module.css";
 import AddProduct from "@/components/templates/p-admin/products/AddProduct";
 import Table from "@/components/templates/p-admin/products/Table";
+import styles from "@/components/templates/p-admin/products/table.module.css"
 
 
-const page = async () => {
+const Page = async () => {
 
-  await connectToDB();
-  const products = await ProductModel.find({}).sort({ _id: -1 }).lean();
+    await connectToDB();
+    const products = await ProductModel.find({}).sort({_id: -1}).lean();
 
 
-  return (
-    <AdminPanelLayout>
-      <main>
-          <AddProduct/>
-        {products.length === 0 ? (
-          <p className={styles.empty}>محصولی وجود ندارد</p>
-        ) : (
-          <Table
-            products={JSON.parse(JSON.stringify(products))}
-            title="لیست محصولات"
-          />
-        )}
-      </main>
-    </AdminPanelLayout>
-  );
+    return (
+        <AdminPanelLayout>
+            <main>
+                <AddProduct/>
+                {products.length === 0 ? (
+                    <p className={styles.empty}>محصولی وجود ندارد</p>
+                ) : (
+                    <Table products={JSON.parse(JSON.stringify(products))} title="لیست محصولات"/>
+                )}
+            </main>
+        </AdminPanelLayout>
+    );
 };
 
-export default page;
+export default Page;
--- src/components/layouts/AdminPanelLayout.js
+++ src/components/layouts/AdminPanelLayout.js
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./adminPanelLayout.module.css";
 import Sidebar from "@/components/modules/p-admin/Sidebar";
 import Topbar from "@/components/modules/p-admin/Topbor";
